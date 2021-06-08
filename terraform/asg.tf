b2ae7a0fc5f436fb79664adc1aecd0f8349699f2
jonny sywulak <jonathan.sywulak@gmail.com>
2021-06-08 08:53:46 +0100

    fmt

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -33,15 +33,15 @@ resource "aws_security_group" "sam-code-test-instance" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = [aws_vpc.main.cidr_block]
-    
+
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
     ipv6_cidr_blocks = ["::/0"]
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 }
 
